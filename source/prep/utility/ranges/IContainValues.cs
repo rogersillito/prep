a42e2c5e74253b490293798f5023babae4530743
Roger Sillito <rogersillito@gmail.com>
2012-11-06 14:01:15 -0700

    Pushing new changes

--- source/prep/utility/ranges/IContainValues.cs
+++ source/prep/utility/ranges/IContainValues.cs
@@ -2,40 +2,45 @@
 
 namespace prep.utility.ranges
 {
-  public interface IContainValues<T> where T : IComparable<T>
-  {
-    bool contains(T value);
-  }
-
-  public class InclusiveRange<T> : IContainValues<T> where T : IComparable<T>
-  {
-    T start;
-    T end;
-
-    public InclusiveRange(T start, T end)
+    public interface IContainValues<T> where T : IComparable<T>
     {
-      this.start = start;
-      this.end = end;
+        bool contains(T value);
     }
 
-    public bool contains(T value)
+    public class InclusiveRange<T> : IContainValues<T> where T : IComparable<T>
     {
-      return value.CompareTo(start) >= 0 && value.CompareTo(end) <= 0;
+        private T start;
+        private T end;
+
+        public InclusiveRange(T start, T end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool contains(T value)
+        {
+            return value.CompareTo(start) >= 0 && value.CompareTo(end) <= 0;
+        }
     }
-  }
 
-  public class RangeWithNoUpperBound<T> : IContainValues<T> where T : IComparable<T>
-  {
-    T start;
-
-    public RangeWithNoUpperBound(T start)
+    public class RangeWithNoUpperBound<T> : IContainValues<T> where T : IComparable<T>
     {
-      this.start = start;
+        private T start;
+
+        public RangeWithNoUpperBound(T start)
+        {
+            this.start = start;
+        }
+
+        public bool contains(T value)
+        {
+            return value.CompareTo(start) > 0;
+        }
     }
 
-    public bool contains(T value)
-    {
-      return value.CompareTo(start) > 0;
-    }
-  }
+    public class Year Range
+}
+
+
 }
