06c2a6622791563a868f69e17b12f395c956a71d
Roger Sillito <rogersillito@gmail.com>
2012-11-07 09:42:28 -0700

    Committing

--- source/prep.specs/code_kata/UnixTimestampConverterTests.cs
+++ source/prep.specs/code_kata/UnixTimestampConverterTests.cs
@@ -1,7 +1,16 @@
-﻿namespace prep.specs.code_kata
+﻿using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
+using prep.code_kata;
+
+namespace prep.specs.code_kata
 {
     public class UnixTimestampConverterTests
     {
-         
+         public class when_a_valid_unix_timestamp_is_supplied: Observes<UnixTimestampConverter>
+         {
+             private Because b = () => sut.ToDateTime("1282352346");
+
+             private It 
+         }
     }
 }
